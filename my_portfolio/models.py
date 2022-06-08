d6177c511328e4c2386e98baf01fd813f5886727
FP <fenerposada@outlook.com>
2022-06-08 17:44:42 -0500

    actualizando 0806

--- my_portfolio/models.py
+++ my_portfolio/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.db.models.fields import CharField,URLField
 from django.db.models.fields.files import ImageField
 from django.core.validators import FileExtensionValidator
-
+import datetime
 class projects(models.Model):
     Title = CharField(max_length= 100)
-    Description = CharField(max_length= 250)
-    Image = ImageField(upload_to= 'portfolio/images')
-    video = models.FileField(upload_to='portfolio/images',null=True, validators=[FileExtensionValidator(allowed_extensions=['MOV','avi','mp4','webm','mkv'])])
+    Short_Description = CharField(max_length= 200)
+    Description = CharField(max_length= 450)
+    Image_1 = ImageField(upload_to= 'portfolio/images')
+    Image_2 = ImageField(upload_to= 'portfolio/images', blank=True)
+    video = models.FileField(upload_to='portfolio/images', blank=True , validators=[FileExtensionValidator(allowed_extensions=['MOV','avi','mp4','webm','mkv'])])
     Url = URLField(blank= True)
     Categoria = CharField(max_length= 250)
-
+    Date = models.DateField(datetime.date.today) 
